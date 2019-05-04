b74a56cbafc133cf15573c14b7d730bc8d811c79
oleg.maliy14@gmail.com <q2301299266>
2019-05-04 13:22:45 +0300

    04/05/2019

---
+++ lesson2/Demo.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) {
+        //java
+        //create class with fields
+        //create object with some values
+        //using it
+
+        Order order = new Order(1001, "tool2212", 140 , new Date(), new Date());
+
+
+        //db
+        //create table with columns
+        //insert some data to the table
+        //using it
+
+    }
+}
---
+++ sql/DATA_for_ORDERS.sql
@@ -0,0 +1,10 @@
+INSERT INTO ORDERS VALUES (1001, 'test1', 100, TO_DATE('2019/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1002, 'test2', 200, TO_DATE('2011/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1003, 'test3', 300, TO_DATE('2012/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1004, 'test4', 100, TO_DATE('2013/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1005, 'test5', 102, TO_DATE('2014/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1006, 'test6', 103, TO_DATE('2015/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1007, 'test7', 104, TO_DATE('2016/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1008, 'test8', 1000, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1009, 'test9', 10, TO_DATE('2018/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO ORDERS VALUES (1010, 'test10', 200, TO_DATE('2019/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'),TO_DATE('2019/05/03 22:00:00','yyyy/mm/dd hh24:mi:ss'));
